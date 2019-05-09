bdc6ff609b6c6d835d11e65b8ff4a53b4a5c126d
weiyilin <weiyilin@baijiahulian.com>
2019-05-09 11:29:39 +0800

    first commit

--- spring-batch/src/test/java/com/wyl/batchdemo/SpringBatchApplicationTests.java
+++ spring-batch/src/test/java/com/wyl/batchdemo/SpringBatchApplicationTests.java
@@ -7,7 +7,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
-public class BatchDemoApplicationTests {
+public class SpringBatchApplicationTests {
 
     @Test
     public void contextLoads() {
